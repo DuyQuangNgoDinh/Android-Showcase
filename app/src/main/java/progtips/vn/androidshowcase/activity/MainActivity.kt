034f337f7456ff7761cff92254a75a8f16466683
nddquang <quang.ngo@2359media.com>
2020-01-15 22:29:35 +0700

    Add Navigation Component

--- app/src/main/java/progtips/vn/androidshowcase/activity/MainActivity.kt
+++ app/src/main/java/progtips/vn/androidshowcase/activity/MainActivity.kt
@@ -2,12 +2,26 @@ package progtips.vn.androidshowcase.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 import progtips.vn.androidshowcase.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+    private val appBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configToolbar()
     }
+
+    private fun configToolbar() {
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
 }
