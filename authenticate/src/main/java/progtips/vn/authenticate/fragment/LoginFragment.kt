4dc1e2f451427a02365bd19c0858eef8d236ce4a
nddquang <quang.ngo@2359media.com>
2020-01-20 11:15:37 +0700

    Add Biometric Authentication

--- authenticate/src/main/java/progtips/vn/authenticate/fragment/LoginFragment.kt
+++ authenticate/src/main/java/progtips/vn/authenticate/fragment/LoginFragment.kt
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -19,6 +23,7 @@ import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.include_social_login.*
 
 import progtips.vn.authenticate.R
+import java.util.concurrent.Executor
 
 /**
  * Login with Google: https://developers.google.com/identity/sign-in/android/start-integrating
@@ -27,6 +32,10 @@ import progtips.vn.authenticate.R
 class LoginFragment : Fragment() {
 
     private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     companion object {
         const val RC_SIGN_IN = 10
@@ -52,6 +61,8 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btnGoogleLogin.setOnClickListener { signIn() }
+
+        biometricLogin()
     }
 
     override fun onStart() {
@@ -89,6 +100,51 @@ class LoginFragment : Fragment() {
     private fun updateUI(account: GoogleSignInAccount?) {
         if (account != null) {
             findNavController().popBackStack()
+        }
+    }
+
+    /**
+     * Biometric Login
+     */
+    private fun biometricLogin() {
+        executor = ContextCompat.getMainExecutor(context)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(context,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(context,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(context, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+        // Prompt appears when user clicks "Log in".
+        // Consider integrating with the keystore to unlock cryptographic operations,
+        // if needed by your app.
+        btnFingerprintLogin.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
         }
     }
 }
